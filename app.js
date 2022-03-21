1e51e66126465206923ca20d678647a0c92e1a85
meghark <98137756+meghark@users.noreply.github.com>
2022-03-21 11:41:49 -0700

    create html template

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+const fs = require('fs');
+const generatePage = require("./src/page-template.js");
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+const[name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem);
-    });
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), 
+                err => {
+                    if(err) throw err;
+                    console.log("Portfolio complete! Check out index.html");
+                } );
